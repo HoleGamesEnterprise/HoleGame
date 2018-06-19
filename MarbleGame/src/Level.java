3612fb04cc90104d1d1647885df876659343746a
DerOtt <4050793+DerOtt@users.noreply.github.com>
2018-06-19 15:13:53 +0200

    Addded some more classes for futher progress

---
+++ MarbleGame/src/Level.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Level {
+
+	
+	ArrayList<GameObjects> go = new ArrayList<GameObjects>();
+	
+	Level(){
+		this(null);
+	}
+	
+	Level(ArrayList<GameObjects> list){
+		go=list;
+	}
+	
+	public void add(GameObjects a) {
+		go.add(a);
+	}
+	
+	public GameObjects remove(GameObjects a) {
+		GameObjects tmp = go.get(go.indexOf(a));
+		go.remove(a);
+		return tmp;
+	}
+	
+	
+}
