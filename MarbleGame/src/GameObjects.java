37709d73e668e579f238f7d0b562bc49fe167a01
DerOtt <4050793+DerOtt@users.noreply.github.com>
2018-06-20 17:15:26 +0200

    A lot of doc

--- MarbleGame/src/GameObjects.java
+++ MarbleGame/src/GameObjects.java
@@ -8,25 +8,49 @@ public class GameObjects {
 	
 	ArrayList<Pair<Integer, Integer>> coords = new ArrayList<Pair<Integer,Integer>>();
 	
+	/**
+	 * creates an gameobject, it already has an arraylist initliazed like dis= ArrayList<Pair<Integer,Integer>>()
+	 */
 	GameObjects(){
 	}
 	
+	/**
+	 * hand over an arraylist of coords if you want to lawl
+	 * @param insert
+	 */
 	GameObjects(ArrayList<Pair<Integer,Integer>> insert){
 		coords = insert;
 	}
 	
+	/**
+	 * returns the Coordslist to the caller. usefull if you dont wanna use my methods lul
+	 * @return
+	 */
 	public ArrayList<Pair<Integer, Integer>> getCoordsList(){
 		return coords;
 	}
 	
+	/**
+	 * if you for some reason need to set the coords at a later date. Use this to replace the full coordslist
+	 * @param insert
+	 */
 	public void setCoordsList(ArrayList<Pair<Integer, Integer>> insert) {
 		coords = insert;
 	}
 	
+	/** add a single coordinate to the object
+	 * 
+	 * @param a
+	 */
 	public void addCoord(Pair<Integer,Integer> a) {
 		coords.add(a);
 	}
 	
+	/**
+	 * removes a single coordinate from the array
+	 * @param a
+	 * @return returns false if the coordinate doesnt exist in the array lmao
+	 */
 	public boolean removeCoord(Pair<Integer,Integer> a) {
 		if (coords.contains(a)) {
 			coords.remove(a);
@@ -37,6 +61,10 @@ public class GameObjects {
 		}
 	}
 	
+	/**
+	 * simple as a cake. 
+	 * @return
+	 */
 	public int getCoordsLength() {
 		return coords.size();
 	}
